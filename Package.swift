676afc35a8dd6178c8414d6a14e34c59ed1cddab
Andrea Stevanato <a.stevanato@doveconviene.com>
2021-01-13 15:59:00 +0100

    resources

--- Package.swift
+++ Package.swift
@@ -20,6 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ASProgressHud",
+            resources: [.process(Assets)],
             dependencies: []),
         .testTarget(
             name: "ASProgressHudTests",
